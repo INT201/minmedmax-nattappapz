042578600e9b83b58d8e0cc322e9894968e440bf
Nattaphat <nattaphat.ngar@kmutt.ac.th>
2022-09-10 13:10:57 +0700

    final

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,39 @@
 const { template } = require('@babel/core')
-
+let max;
+let min;
+let mid;
 function minMedMax(n1, n2, n3) {
-  //code here
+  if(n1>n2 && n1>n3){
+    max = n1
+    if(n3>n2){
+        mid = n3
+        min = n2
+    }else{
+       mid = n2
+       min = n3
+    }
+  }
+  if(n2>n1 && n2>n3){
+    max = n2
+    if(n1>n3){
+      mid = n1
+      min = n3
+    }else{
+      min = n3
+      mid = n1
+    }
+  }
+  if(n3>n1 && n3>n2){
+    max = n3
+    if(n2>n1){
+      mid = n2
+      min = n1
+    }else{
+      mid = n1
+      min = n2
+    }
+  }
+  return {"min":min,"mid":mid,"max":max}
 }
 module.exports = minMedMax
+
